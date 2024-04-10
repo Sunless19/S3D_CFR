587976e637b008075cbed75f2aa14611f37c7bca
thatBeatrice <morarubeatrice77@yahoo.com>
2024-04-10 14:25:21 +0300

    Added camera and shader

---
+++ Project/Project/Camera.cpp
@@ -0,0 +1,187 @@
+#include "Camera.h"
+
+Camera::Camera(const int width, const int height, const glm::vec3& position)
+{
+    startPosition = position;
+    freeCamera = true;
+    Set(width, height, position);
+}
+
+void Camera::Set(const int width, const int height, const glm::vec3& position)
+{
+    this->isPerspective = true;
+    this->yaw = YAW;
+    this->pitch = PITCH;
+
+    this->FoVy = FOV;
+    this->width = width;
+    this->height = height;
+    this->zNear = zNEAR;
+    this->zFar = zFAR;
+
+    this->worldUp = glm::vec3(0, 1, 0);
+    this->position = position;
+
+    lastX = width / 2.0f;
+    lastY = height / 2.0f;
+    bFirstMouseMove = true;
+
+    UpdateCameraVectors();
+}
+
+void Camera::Reset(const int width, const int height)
+{
+    Set(width, height, startPosition);
+}
+
+void Camera::Reshape(int windowWidth, int windowHeight)
+{
+    width = windowWidth;
+    height = windowHeight;
+
+    // define the viewport transformation
+    glViewport(0, 0, windowWidth, windowHeight);
+}
+
+const glm::vec3 Camera::GetPosition() const
+{
+    return position;
+}
+
+void Camera::SetPosition(glm::vec3 position)
+{
+    this->position = position;
+}
+
+//glm::mat4 Camera::GetViewMatrix(MoveableObject* model)
+//{
+//    if (freeCamera)
+//        return glm::lookAt(position, position + forward, up);
+//    float distance = 10.0f;
+//    this->forward.x = cos(glm::radians(model->GetYaw())) * distance;
+//    this->forward.z = sin(glm::radians(model->GetYaw())) * distance;
+//    return glm::lookAt(model->GetPosition() + glm::vec3(-forward.x, forward.y, -forward.z) + glm::vec3(0.f, 5.0f, 0.f), model->GetPosition() + glm::vec3(0.f, 3.0f, 0.f), glm::vec3(0.0f, 1.0f, 0.0f));
+//}
+
+const glm::mat4 Camera::GetProjectionMatrix() const
+{
+    glm::mat4 Proj = glm::mat4(1);
+    if (isPerspective)
+    {
+        float aspectRatio = ((float)(width)) / height;
+        Proj = glm::perspective(glm::radians(FoVy), aspectRatio, zNear, zFar);
+    }
+    else
+    {
+        float scaleFactor = 2000.f;
+        Proj = glm::ortho<float>(
+            -width / scaleFactor, width / scaleFactor,
+            -height / scaleFactor, height / scaleFactor, -zFar, zFar);
+    }
+    return Proj;
+}
+
+void Camera::ProcessKeyboard(ECameraMovementType direction, float deltaTime)
+{
+    float velocity = (float)(cameraSpeedFactor * deltaTime);
+    switch (direction)
+    {
+    case ECameraMovementType::FORWARD:
+        position += forward * velocity;
+        break;
+    case ECameraMovementType::BACKWARD:
+        position -= forward * velocity;
+        break;
+    case ECameraMovementType::LEFT:
+        position -= right * velocity;
+        break;
+    case ECameraMovementType::RIGHT:
+        position += right * velocity;
+        break;
+    case ECameraMovementType::UP:
+        position += up * velocity;
+        break;
+    case ECameraMovementType::DOWN:
+        position -= up * velocity;
+        break;
+    }
+}
+
+void Camera::MouseControl(float xPos, float yPos)
+{
+    if (bFirstMouseMove)
+    {
+        lastX = xPos;
+        lastY = yPos;
+        bFirstMouseMove = false;
+    }
+
+    float xChange = xPos - lastX;
+    float yChange = lastY - yPos;
+    lastX = xPos;
+    lastY = yPos;
+
+    if (fabs(xChange) <= 1e-6 && fabs(yChange) <= 1e-6)
+    {
+        return;
+    }
+    xChange *= mouseSensitivity;
+    yChange *= mouseSensitivity;
+
+    ProcessMouseMovement(xChange, yChange);
+}
+
+void Camera::ProcessMouseScroll(float yOffset)
+{
+    if (FoVy >= 1.0f && FoVy <= 90.0f)
+    {
+        FoVy -= yOffset;
+    }
+    if (FoVy <= 1.0f)
+        FoVy = 1.0f;
+    if (FoVy >= 90.0f)
+        FoVy = 90.0f;
+}
+
+void Camera::SetForwardVector(glm::vec3 forward)
+{
+    this->forward = forward;
+}
+
+void Camera::ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch)
+{
+    yaw += xOffset;
+    pitch += yOffset;
+
+    if (constrainPitch)
+    {
+        if (pitch > 89.0f)
+            pitch = 89.0f;
+        if (pitch < -89.0f)
+            pitch = -89.0f;
+    }
+
+    UpdateCameraVectors();
+}
+
+void Camera::SetFreeCamera(bool freeCamera)
+{
+    this->freeCamera = freeCamera;
+}
+
+void Camera::UpdateCameraVectors()
+{
+    // Calculate the new forward vector
+    this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    this->forward.y = sin(glm::radians(pitch));
+    this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    this->forward = glm::normalize(this->forward);
+    // Also re-calculate the Right and Up vector
+    right = glm::normalize(glm::cross(forward, worldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+    up = glm::normalize(glm::cross(right, forward));
+}
+
+bool Camera::GetFreeCamera()
+{
+    return this->freeCamera;
+}
--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,20 +1,89 @@
-// Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <GL/glew.h>
+
+#define GLM_FORCE_CTOR_INIT 
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main()
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "Camera.h"
+#include "Shader.h"
+
+#pragma comment (lib, "glfw3dll.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "OpenGL32.lib")
+
+const unsigned int SCR_WIDTH = 1800;
+const unsigned int SCR_HEIGHT = 1080;
+bool isDayTime = false;
+
+
+Camera* pCamera = nullptr;
+
+unsigned int CreateTexture(const std::string& strTexturePath)
 {
-    std::cout << "Hello World!\n";
+	unsigned int textureId = -1;
+
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load(strTexturePath.c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrChannels == 1)
+			format = GL_RED;
+		else if (nrChannels == 3)
+			format = GL_RGB;
+		else if (nrChannels == 4)
+			format = GL_RGBA;
+
+		glGenTextures(1, &textureId);
+		glBindTexture(GL_TEXTURE_2D, textureId);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	}
+	else
+	{
+		std::cout << "Failed to load texture: " << strTexturePath << std::endl;
+	}
+	stbi_image_free(data);
+
+	return textureId;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+
+void renderScene(const Shader& shader);
+void renderFloor();
+
+double deltaTime = 0.0f; // time between current frame and last frame
+double lastFrame = 0.0f;
+
+

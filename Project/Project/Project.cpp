a25104d47349746d9ac1fbbc1a65197a6532f0b4
Andrei Pavel <andrei-va.pavel@student.unitbv.ro>
2024-05-15 10:14:56 +0300

    Day/Night Mode + comments removed

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -172,13 +172,11 @@ std::vector<glm::vec3> forestTreePositions =
 	glm::vec3(-40.0f, -1.5f, -10.0f),
 	glm::vec3(-40.0f, -1.5f, -70.0f),
 	glm::vec3(-40.0f, -1.5f, -140.0f),
-	//glm::vec3(-40.0f, -1.5f, -210.0f),
 
 	glm::vec3(40.0f, -1.5f, 50.0f),
 	glm::vec3(40.0f, -1.5f, -10.0f),
 	glm::vec3(40.0f, -1.5f, -70.0f),
 	glm::vec3(40.0f, -1.5f, -140.0f),
-	//glm::vec3(40.0f, -1.5f, -210.0f),
 };
 
 std::vector<glm::vec3> treePositions =
@@ -194,78 +192,26 @@ std::vector<glm::vec3> treePositions =
 	glm::vec3(40.0f, -1.5f, -70.0f),
 	glm::vec3(40.0f, -1.5f, -140.0f),
 	glm::vec3(40.0f, -1.5f, -210.0f),
-
-	//glm::vec3(43.0f, -1.5f, -45.0f),
-	//glm::vec3(51.0f, -1.5f, 20.0f),
-	//glm::vec3(-36.5f, -1.5f, -13.0f),
-	//glm::vec3(12.0f, -1.5f, 52.0f),
-	//glm::vec3(110.0f, -1.5f, -83.0f),
-	//glm::vec3(-24.0f, -1.5f, 18.0f),
-	//glm::vec3(48.0f, -1.5f, -20.0f),
-	//glm::vec3(-63.0f, -1.5f, 15.0f),
-	//glm::vec3(76.0f, -1.5f, -10.0f),
-	//glm::vec3(-85.0f, -1.5f, -25.0f),
-	//glm::vec3(100.5f, -1.5f, 31.0f),
 };
 
 std::vector<glm::vec3> mountainsPositions =
 {
 	glm::vec3(50.0f, -1.55f, -25.0f),
 	glm::vec3(-50.0f, -1.55f, -35.0f),
-	//glm::vec3(-20.5f, -1.55f, 10.0f),
-	//glm::vec3(20.0f, -1.55f, 20.0f),
-	//glm::vec3(-35.0f, -1.55f, -15.0f),
-	//glm::vec3(-25.0f, -1.55f, -30.0f),
-	//far
-	/*glm::vec3(-30.0f, -1.55f, -150.0f),
-	glm::vec3(-15.0f, -1.55f, -145.0f),
-	glm::vec3(15.0f, -1.55f, -155.0f),*/
-
-	////far far
-	//glm::vec3(70.0f, -1.55f, 200.0f),
-	//glm::vec3(-70.0f, -1.55f, 200.0f),
-	//glm::vec3(90.0f, -1.55f, 160.0f),
-
-	//glm::vec3(160.0f, -1.55f, 100.0f),
-	//glm::vec3(-160.0f, -1.55f, 100.0f)
 };
 
 std::vector<glm::vec3> mountainsScales =
 {
-	//glm::vec3(0.05f),
 	glm::vec3(0.8f),
-	//glm::vec3(0.05f),
-	//glm::vec3(0.1f),
-	//glm::vec3(0.1f),
 	glm::vec3(0.6f),
-	//far
-	/*glm::vec3(0.6f),
-	glm::vec3(0.8f),
-	glm::vec3(0.7f),*/
-	////far far
-	//glm::vec3(0.6f),
-	//glm::vec3(0.6f),
-	//glm::vec3(0.8f),
-
-	//glm::vec3(0.4f),
-	//glm::vec3(0.4f)
 };
-
-//std::vector<glm::vec3> stationsPositions =
-//{
-//	glm::vec3(50.0f, -1.55f, -125.0f),
-//	glm::vec3(-50.0f, -1.55f, -135.0f),
-//};
 
 float scaleFactor = 2.0f; // You can adjust this value according to your needs
 
-//std::vector<glm::vec3> trainPosition =
-//{
-//	glm::vec3(0.0f, -1.5f, 0.0f)
-//};
-
 Model railModel, trainModel, treeModel, mountainModel, stationModel, benchModel, humanModel, warModel, forestModel, BucurestiModel, PloiestiModel, SinaiaModel, BrasovModel;
 MoveableObject trainVehicle, railVehicle;
+
+bool night = false;
 
 std::vector<std::string> facesDay
 {
@@ -296,6 +242,9 @@ float trainX = 0.0f;
 float trainZ = 0.0f;
 
 int stationCount = 0;
+
+unsigned int cubemapTexture;
+unsigned int cubemapTextureNight;
 
 int main(int argc, char** argv)
 {
@@ -338,19 +287,14 @@ int main(int argc, char** argv)
 
 	glEnable(GL_DEPTH_TEST);
 
-	// build and compile shaders
-	// -------------------------
+
 	Shader shadowMappingShader("ShadowMapping.vs", "ShadowMapping.fs");
 	Shader shadowMappingDepthShader("ShadowMappingDepth.vs", "ShadowMappingDepth.fs");
 	Shader ModelShader("ModelShader.vs", "ModelShader.fs");
 	Shader skyboxShader("skybox.vs", "skybox.fs");
 
-	// load textures
-	//// -------------
 	unsigned int floorTexture = CreateTexture("skybox_images\\grass.jpg");
 
-	// configure depth map FBO
-	// -----------------------
 	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
 	unsigned int depthMapFBO;
 	glGenFramebuffers(1, &depthMapFBO);
@@ -380,8 +324,6 @@ int main(int argc, char** argv)
 	shadowMappingShader.SetInt("diffuseTexture", 0);
 	shadowMappingShader.SetInt("shadowMap", 1);
 
-	// lighting info
-	// -------------
 	glm::vec3 lightPos(0.0f, 100.0f, 0.0f);
 
 	glEnable(GL_CULL_FACE);
@@ -401,9 +343,9 @@ int main(int argc, char** argv)
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	unsigned int cubemapTexture;
+	//unsigned int cubemapTexture;
 
-	unsigned int cubemapTextureNight;
+	//unsigned int cubemapTextureNight;
 
 	glGenTextures(1, &cubemapTexture);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
@@ -479,7 +421,6 @@ int main(int argc, char** argv)
 
 	//Rail model loading
 	railModel = Model("Assets\\Rail\\rail.obj");
-	//railVehicle = MoveableObject(railModel, SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0f, 5.0f, 0.0f));
 
 	//Tree model loading
 	treeModel = Model("Assets\\Tree\\Tree.obj");
@@ -502,19 +443,6 @@ int main(int argc, char** argv)
 
 	//Human model loading
 	humanModel = Model("Assets\\Human\\Humano_01Business_01_30K.obj");
-
-
-	// Grass vertices
-	//float grassVertices[] = {
-	//	// positions          // texture Coords 
-	//	0.5f,  0.5f, 0.0f,  1.0f, 0.0f,
-	//   -0.5f,  0.5f, 0.0f,  0.0f, 0.0f,
-	//   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,
-
-	//	0.5f,  0.5f, 0.0f,  1.0f, 0.0f,
-	//   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,
-	//	0.5f, -0.5f, 0.0f,  1.0f, 1.0f
-	//};
 
 	//// Grass VAO si VBO
 	//unsigned int grassVAO, grassVBO;
@@ -568,15 +496,13 @@ int main(int argc, char** argv)
 		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTextureNight);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
-		glDepthFunc(GL_LESS); // set depth function back to default
+		glDepthFunc(GL_LESS);
 		glDepthMask(GL_TRUE);
 		double currentFrame = glfwGetTime();
 
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		// input
-		// -----
 		processInput(window);
 
 		// Terrain update
@@ -597,8 +523,6 @@ int main(int argc, char** argv)
 			}
 		}
 
-		// render
-		// ------
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -619,7 +543,6 @@ int main(int argc, char** argv)
 		lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
 		lightSpaceMatrix = lightProjection * lightView;
 
-		// render scene from light's point of view
 		shadowMappingDepthShader.Use();
 		shadowMappingDepthShader.SetMat4("lightSpaceMatrix", lightSpaceMatrix);
 
@@ -630,47 +553,6 @@ int main(int argc, char** argv)
 		glEnable(GL_CULL_FACE);
 		glCullFace(GL_FRONT);
 		renderScene(shadowMappingDepthShader);
-
-		//Grass
-
-		//glBindVertexArray(grassVAO);
-		//glBindTexture(GL_TEXTURE_2D, grassTexture);
-
-		//for (float row = -4.5f; row <= 4.5f; row += 0.5f)
-		//	for (float column = -4.5f; column <= 4.5f; column += 0.5f)
-		//	{
-		//		glm::mat4 worldTransf = glm::translate(glm::mat4(1.0), glm::vec3(row, 0.0f, column));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		worldTransf = glm::rotate(worldTransf, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		worldTransf = glm::rotate(worldTransf, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		worldTransf = glm::rotate(worldTransf, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		worldTransf = glm::rotate(worldTransf, glm::radians(105.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		worldTransf = glm::rotate(worldTransf, glm::radians(75.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		worldTransf = glm::rotate(worldTransf, glm::radians(120.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		//		/*worldTransf = glm::rotate(worldTransf, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//		shaderBlending.SetMat4("model", worldTransf);
-		//		glDrawArrays(GL_TRIANGLES, 0, 6);*/
-		//	}
 
 		//---TRAIN VARIABLES---
 		float trainRotation = -10.0f;
@@ -684,6 +566,13 @@ int main(int argc, char** argv)
 		float treeRotation = 0.0f;
 		glm::vec3 treeScale = glm::vec3(1);
 
+		//----MOUNTAIN VARIABLES---
+		float mountainRotation = 0.0f;
+		glm::vec3 mountainScale = glm::vec3(0.1f);
+		for (int i = 0; i < mountainsPositions.size(); i++)
+		{
+			renderModel(shadowMappingDepthShader, mountainModel, mountainsPositions[i] + glm::vec3(0.0f, 0.0f, chunkBorder + 100.0f), mountainRotation, mountainsScales[i]);
+		}
 
 		//TRAIN AND RAILS SHADOW RENDERING
 
@@ -699,23 +588,11 @@ int main(int argc, char** argv)
 		//	renderModel(shadowMappingDepthShader, railModel, railPosition, railRotation, railScale);
 		//}
 
-		float mountainRotation = 0.0f;
-		glm::vec3 mountainScale = glm::vec3(0.1f);
-
-		for (int i = 0; i < mountainsPositions.size(); i++)
-		{
-			renderModel(shadowMappingDepthShader, mountainModel, mountainsPositions[i] + glm::vec3(0.0f, 0.0f, chunkBorder + 100.0f), mountainRotation, mountainsScales[i]);
-		}
-
-		//renderModel(shadowMappingDepthShader, stationModel, glm::vec3(0.0f, 0.0f, 0.0f), 0.0f, glm::vec3(1.0f));
-
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shadowMappingShader.Use();
@@ -729,8 +606,6 @@ int main(int argc, char** argv)
 		shadowMappingShader.SetVec3("lightPos", lightPos);
 		shadowMappingShader.SetMat4("lightSpaceMatrix", lightSpaceMatrix);
 		glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, floorTexture);
-		//glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glDisable(GL_CULL_FACE);
 		renderScene(shadowMappingShader);
@@ -738,18 +613,12 @@ int main(int argc, char** argv)
 
 		//-----TRAIN AND RAILS RENDERING------
 		renderModel(ModelShader, trainVehicle.GetVehicleModel(), trainVehicle.GetPosition(), trainVehicle.GetRotation() - 1, trainScale);
-		//for (auto& trainPosition : trainPosition)
-		//{
-		//	renderModel(ModelShader, trainModel, trainPosition - glm::vec3(0.0f, 0.0f, 0.0f), trainVehicle, trainScale);
-		//}
-
-		//renderModel(ModelShader, railVehicle.GetVehicleModel(), railVehicle.GetPosition(), railVehicle.GetRotation(), railScale);
 		for (int i = 0; i < railPositions.size(); i++)
 		{
 			renderModel(ModelShader, railModel, railPositions[i] + glm::vec3(0.0f, 0.0f, chunkBorder + 50.0f), railRotation, railScale);
 		}
 
-
+		//-----GENERATIVE RENDERING-----
 		switch (hasStation)
 		{
 		case true:
@@ -806,14 +675,9 @@ int main(int argc, char** argv)
 			break;
 		}
 
-
-
-		//stationModel.Draw(ModelShader);
-		//-------------------------------------
-
-		glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f); // White light
-		glm::vec3 lightDir = glm::normalize(glm::vec3(-0.2f, -1.0f, -0.3f)); // Example direction
-		glm::vec3 objectColor = glm::vec3(1.0f, 0.5f, 0.31f); // Example color (rust)
+		glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+		glm::vec3 lightDir = glm::normalize(glm::vec3(-0.2f, -1.0f, -0.3f));
+		glm::vec3 objectColor = glm::vec3(1.0f, 0.5f, 0.31f);
 
 		ModelShader.Use();
 		ModelShader.SetVec3("lightColor", lightColor);
@@ -854,8 +718,6 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-// renders the 3D scene
-// --------------------
 void renderScene(const Shader& shader)
 {
 	glm::mat4 model;
@@ -864,14 +726,12 @@ void renderScene(const Shader& shader)
 	shader.SetMat4("model", model);
 }
 
-
 unsigned int modelVAO = 0;
 unsigned int modelVBO = 0;
 unsigned int modelEBO;
 
 void renderModel(Shader& ourShader, Model& ourModel, const glm::vec3& position, float rotationAngle, const glm::vec3& scale)
 {
-
 	ourShader.Use();
 
 	glm::mat4 model = glm::mat4(1.0f);
@@ -893,28 +753,19 @@ bool isTrainSoundPlaying = false;
 
 void processInput(GLFWwindow* window)
 {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	//-----------------NIGHT MODE-----------------
 	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
 	{
-		blendFactor = std::min(blendFactor + 0.001, 1.0);
-		ambientFactor = std::max(ambientFactor - 0.001, 0.34);
+		blendFactor = std::min(blendFactor + 0.01, 1.0);
+		ambientFactor = std::max(ambientFactor - 0.01, 0.34);
 	}
 	if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
 	{
-		blendFactor = std::max(blendFactor - 0.001, 0.0);
-		ambientFactor = std::min(ambientFactor + 0.001, 0.9);
-	}
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
-	{
-		freeCameraView = false;
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
-	{
-		freeCameraView = true;
-		//pCamera->Reset(SCR_WIDTH, SCR_HEIGHT);
-		pCamera->SetFreeCamera(true);
+		blendFactor = std::max(blendFactor - 0.01, 0.0);
+		ambientFactor = std::min(ambientFactor + 0.01, 0.9);
 	}
 
 	//-----------------TRAIN MOVEMENT-----------------
@@ -949,8 +800,16 @@ void processInput(GLFWwindow* window)
 		trainVehicle.setSpeed(12.5);
 	}
 
-	//------------------------------------------------
-
+	//-----------------CAMERA MODE---------------------
+	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
+	{
+		freeCameraView = false;
+	}
+	if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+	{
+		freeCameraView = true;
+		pCamera->SetFreeCamera(true);
+	}
 
 	//-----------------FREE CAMERA MOVEMENT-----------------
 	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS && freeCameraView == true)
